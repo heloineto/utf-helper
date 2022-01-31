d0764562fc4e49202d59cd78b3b08ee307c99d87
heloineto <hey@heloineto.com>
2022-01-31 16:20:27 -0300

    updated theme

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,29 +1,22 @@
-//@ts-nocheck
-
-import { createTheme } from '@mui/material/styles';
+import { createTheme, ThemeOptions } from '@mui/material/styles';
 import resolveConfig from 'tailwindcss/resolveConfig';
 import tailwindConfigModule from '../tailwind.config.js';
 
-const tailwindConfig = resolveConfig(tailwindConfigModule);
+const { theme: TwTheme } = resolveConfig(tailwindConfigModule);
+const themeOptions: ThemeOptions = {};
 
-const theme = createTheme({
-  breakpoints: {
-    values: {
-      sm: tailwindConfig.theme.screens.sm,
-      md: tailwindConfig.theme.screens.md,
-      lg: tailwindConfig.theme.screens.lg,
-      xl: tailwindConfig.theme.screens.xl,
-      '2xl': tailwindConfig.theme.screens['2xl'],
-    },
-  },
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-  },
-});
+if (TwTheme.screens) {
+  themeOptions.breakpoints = {
+    values: Object.entries(TwTheme.screens).reduce(
+      (acc, [key, value]) => ({ ...acc, [key]: parseInt(value) }),
+      {}
+    ) as any,
+  };
+}
+
+if (TwTheme.colors) {
+}
+
+const theme = createTheme(themeOptions);
 
 export default theme;
