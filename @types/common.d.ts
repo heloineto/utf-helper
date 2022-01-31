8cb9ade3b76d99afb19b4525691e09cc46be640f
heloineto <hey@heloineto.com>
2022-01-31 15:50:53 -0300

    added mui 5

--- @types/common.d.ts
+++ @types/common.d.ts
@@ -5,11 +5,3 @@ type SVGProps<A> = import('react').SVGProps<A>;
 type CSSProperties = import('react').CSSProperties;
 
 type NextPage = import('next').NextPage;
-
-// type DateTime = import('luxon').DateTime;
-
-// type FirebaseRef = import('firebase/firestore').DocumentReference<
-//   import('firebase/firestore').DocumentData
-// >;
-// type FieldValue = import('firebase/firestore').FieldValue;
-// type Timestamp = import('firebase/firestore').Timestamp;
---
+++ @types/declarations.d.ts
@@ -0,0 +1,10 @@
+declare module '@material-ui/core/styles' {
+  interface BreakpointOverrides {
+    xs: false;
+    sm: true;
+    md: true;
+    lg: true;
+    xl: true;
+    '2xl': true;
+  }
+}
