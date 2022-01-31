b07a43863c13111cd1f1d7be2a5e6562995b0ff6
heloineto <hey@heloineto.com>
2022-01-31 15:16:21 -0300

    installed tailwind and typescript

---
+++ @types/common.d.ts
@@ -0,0 +1,15 @@
+type ReactNode = import('react').ReactNode;
+type SetStateAction<A> = import('react').SetStateAction<A>;
+type Dispatch<A> = import('react').Dispatch<A>;
+type SVGProps<A> = import('react').SVGProps<A>;
+type CSSProperties = import('react').CSSProperties;
+
+type NextPage = import('next').NextPage;
+
+// type DateTime = import('luxon').DateTime;
+
+// type FirebaseRef = import('firebase/firestore').DocumentReference<
+//   import('firebase/firestore').DocumentData
+// >;
+// type FieldValue = import('firebase/firestore').FieldValue;
+// type Timestamp = import('firebase/firestore').Timestamp;
