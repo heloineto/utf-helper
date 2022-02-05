b9985dad4f3a39cb231c65f8466b94422d2beca5
heloineto <hey@heloineto.com>
2022-02-05 20:40:41 -0300

    added CustomScrollBar

---
+++ components/styledJsx/CustomScrollBar.tsx
@@ -0,0 +1,29 @@
+const CustomScrollBar = () => {
+  return (
+    <style global jsx>{`
+      ::-webkit-scrollbar {
+        width: 0.5rem;
+        height: 0.5rem;
+      }
+
+      ::-webkit-scrollbar-track {
+        background: transparent;
+      }
+
+      ::-webkit-scrollbar-thumb {
+        background: #f5f5f5;
+        border-radius: 1rem;
+      }
+
+      *:hover > ::-webkit-scrollbar-thumb {
+        background: #d6d6d6;
+      }
+
+      ::-webkit-scrollbar-thumb:active {
+        background: #767676;
+      }
+    `}</style>
+  );
+};
+
+export default CustomScrollBar;
