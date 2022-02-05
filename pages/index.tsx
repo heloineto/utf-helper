c8ceb72174a60ad5aa5d013082089e427ecfe61a
heloineto <hey@heloineto.com>
2022-02-05 18:14:56 -0300

    updated Divider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import MainShell from '@components/appShells/MainShell';
+import Divider from '@components/elements/Divider';
 import Schedule from '@components/Schedule';
 
 const Home: NextPage = () => {
   return (
     <MainShell>
       <div className="bg-red-200" style={{ height: `50%` }}></div>
+      <Divider />
       <div className="bg-blue-200" style={{ height: `50%` }}>
         {/* <Schedule /> */}
       </div>
