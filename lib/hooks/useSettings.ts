845ee0eed93655a1d3e6f35a23bcac18c605efa6
heloineto <hey@heloineto.com>
2022-02-06 13:03:31 -0300

    updated useSettings

--- lib/hooks/useSettings.ts
+++ lib/hooks/useSettings.ts
@@ -1,8 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const useSettings = () => {
   const [theme, setTheme] = useState<Theme>('light');
   const [direction, setDirection] = useState<Direction>('vertical');
+
+  useEffect(() => {
+    const settingsStr = localStorage.getItem('settings');
+    if (!settingsStr) return;
+
+    let settings;
+    try {
+      settings = JSON.parse(settingsStr);
+    } catch (error) {
+      if (true) {
+      }
+    }
+
+    if (!settings) return;
+
+    setTheme(settings.theme);
+    setDirection(settings.direction);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('settings', JSON.stringify({ theme, direction }));
+  }, [theme, direction]);
 
   return { theme, setTheme, direction, setDirection };
 };
