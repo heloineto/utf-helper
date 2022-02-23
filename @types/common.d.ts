afe0b79f51fe74ec9805c67a1288b86747a9a741
Heloi Neto <hey@heloineto.com>
2022-02-23 11:38:02 -0300

    updated Month

--- @types/common.d.ts
+++ @types/common.d.ts
@@ -1,11 +1,10 @@
 type ReactNode = import('react').ReactNode;
-type SetStateAction<A> = import('react').SetStateAction<A>;
-type Dispatch<A> = import('react').Dispatch<A>;
 type SVGProps<A> = import('react').SVGProps<A>;
 type CSSProperties = import('react').CSSProperties;
-type RefObject<A> = import('react').RefObject<a>;
-type ComponentProps<A> = import('react').ComponentProps<a>;
-//! Check for where to import the proper definition of this type
+type RefObject<A> = import('react').RefObject<A>;
+type ComponentProps<A> = import('react').ComponentProps<A>;
+type SetStateAction<A> = import('react').SetStateAction<A>;
+type Dispatch<A> = import('react').Dispatch<A>;
 type SetValue<A> = Dispatch<SetStateAction<A>>;
 
 type NextPage = import('next').NextPage;
--- components/elements/calendar/Month/Month.tsx
+++ components/elements/calendar/Month/Month.tsx
@@ -1,20 +1,24 @@
 import clsx from 'clsx';
 import { DateTime } from 'luxon';
+import { useMemo } from 'react';
 
 import MonthHeader from './Month.Header';
 import MonthTable from './Month.Table';
 
-interface Props {
-  className?: string;
+interface Props extends ComponentProps<'div'> {
   month?: number;
   year?: number;
 }
 
 const Month = ({ month, year, className }: Props) => {
-  const date = DateTime.fromObject({ month, year }) || DateTime.now();
-  if (!date.isValid) throw new Error(`${date.invalidReason}. ${date.invalidExplanation}`);
+  const date = useMemo(
+    () => DateTime.fromObject({ month, year }) || DateTime.now(),
+    [DateTime, month, year]
+  );
 
-  console.log(date);
+  if (!date.isValid) {
+    throw new Error(`${date.invalidReason}. ${date.invalidExplanation}`);
+  }
 
   return (
     <div className={clsx(className, 'bg-gray-800 rounded-xl')}>
