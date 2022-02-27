392d136db7817d40ccf9bc30169b2404cae14ced
Heloi Neto <hey@heloineto.com>
2022-02-27 15:44:59 -0300

    updated MainShell

--- components/appShells/MainShell.tsx
+++ components/appShells/MainShell.tsx
@@ -1,29 +1,14 @@
 import Navbar from '@components/navigation/Navbar';
-import { UserDataContext } from '@lib/context';
-import { useContext } from 'react';
 
 interface Props {
   children: ReactNode;
 }
 
 const MainShell = ({ children }: Props) => {
-  const userData = useContext(UserDataContext);
-
   return (
     <div className="h-screen flex flex-col">
       <Navbar className="flex-shrink-0" />
       <main className="overflow-hidden">{children}</main>
-      {/* 
-      //!
-      <CampusAndCourseDialog
-        open={dialogOpen}
-        onClose={() => {
-          if (!campus || !course) return;
-          setDialogOpen(false);
-        }}
-        campus={campus}
-        course={course}
-      /> */}
     </div>
   );
 };
