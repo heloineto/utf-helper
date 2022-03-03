4815f90f59aebb5217794edcabdc189ef1e3b6d8
Heloi Neto <hey@heloineto.com>
2022-03-02 22:24:21 -0300

    added Agenda

---
+++ pages/agenda.tsx
@@ -0,0 +1,11 @@
+import MainShell from '@components/appShells/MainShell';
+
+const Agenda: NextPage = () => {
+  return (
+    <MainShell>
+      <div></div>
+    </MainShell>
+  );
+};
+
+export default Agenda;
