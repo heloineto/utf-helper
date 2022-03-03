5ab2bb00cdd41d5e05e5f9495d974534fdefb2b3
Heloi Neto <hey@heloineto.com>
2022-03-03 07:50:20 -0300

    updated MonthCellHighlight

--- components/pageComponents/calendar/Month/Month.CellHighlight.tsx
+++ components/pageComponents/calendar/Month/Month.CellHighlight.tsx
@@ -12,19 +12,26 @@ type Props = {
 const MonthCellHighlight = ({ isToday, isSelected }: Props) => {
   const { darkMode } = useContext(SettingsContext);
   const highlightColors = useMemo(() => {
-    const twColor = isToday ? twColors['sky'] : twColors['indigo'];
-    const color = colord(darkMode ? twColor[600] : twColor[300]);
+    let hexColor = '#000000';
+
+    if (isToday) {
+      hexColor = darkMode ? twColors['sky'][600] : twColors['sky'][300];
+    } else if (isSelected) {
+      hexColor = darkMode ? twColors['indigo'][800] : twColors['indigo'][800];
+    }
+
+    const color = colord(hexColor);
 
     return [color.alpha(0.3).toRgbString(), color.alpha(0.4).toRgbString()];
-  }, []);
+  }, [darkMode, isToday, isSelected]);
 
   return (
     <div
       className={classNames(
         !isToday && !isSelected && 'hidden',
-        isToday && 'ring-2 ring-inset ring-sky-500',
-        isSelected && 'ring-2 ring-inset ring-indigo-500',
-        'absolute w-full h-full top-0 left-0 rounded-md'
+        isToday && '!ring-sky-500',
+        isSelected && 'ring-indigo-500',
+        'absolute w-full h-full top-0 left-0 ring-2 ring-inset rounded-md'
       )}
       style={{
         background: `repeating-linear-gradient(45deg, ${highlightColors[0]}, ${highlightColors[0]} 0.25rem, ${highlightColors[1]} 0.25rem, ${highlightColors[1]} 0.5rem)`,
