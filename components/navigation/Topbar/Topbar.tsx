c2d14b68e18952d689d1407dc9ba1cf03499f61d
Heloi Neto <hey@heloineto.com>
2022-03-04 09:31:07 -0300

    updated Navbar

--- components/navigation/Topbar/Topbar.tsx
+++ components/navigation/Topbar/Topbar.tsx
@@ -7,7 +7,7 @@ const Topbar = ({ className, ...divProps }: Props) => {
     <div
       className={classNames(
         className,
-        'w-full flex-shrink-0 h-12 px-5 bg-slate-50 dark:bg-slate-900/50 flex flex-col sm:flex-row justify-between shadow-sm lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06]'
+        'w-full flex-shrink-0 h-12 px-5 bg-slate-50 dark:bg-slate-900/50 flex flex-col sm:flex-row justify-between shadow-sm lg:border-b lg:pb-px lg:border-slate-900/10 dark:border-slate-50/[0.06]'
       )}
       {...divProps}
     />
