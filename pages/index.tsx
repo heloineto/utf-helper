0b11fda1976fe44bdf2068a7f960855713c54116
Heloi Neto <hey@heloineto.com>
2022-03-04 09:07:23 -0300

    updated Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import MainShell from '@components/appShells/MainShell';
+import { Logomark } from '@components/decoration/logo';
 
 const Home: NextPage = () => {
   return (
     <MainShell>
       <div className="bg-slate-100 dark:bg-slate-800 p-5 h-full overflow-y-scroll">
-        <div></div>
+        <div className="w-full flex justify-center items-center">
+          <Logomark />
+        </div>
       </div>
     </MainShell>
   );
