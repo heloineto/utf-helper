c18e073033307bf522ee4cdfd6b7039dbfe93d02
Heloi Neto <hey@heloineto.com>
2022-03-04 15:54:21 -0300

    commit before big changes

--- @types/firestore/users.d.ts
+++ @types/firestore/users.d.ts
@@ -2,6 +2,8 @@ interface UserDetails {
   email?: string;
   photoUrl?: string;
   name?: string;
+  campus?: string;
+  course?: string;
 }
 
 type UserDetailsWithRef = UserDetails & { ref: FirebaseRef };
