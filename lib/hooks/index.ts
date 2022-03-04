69609f472f3535b9eddca01f4840ef86c16e3864
Heloi Neto <hey@heloineto.com>
2022-03-04 11:39:40 -0300

    updated hooks imports

--- lib/hooks/index.ts
+++ lib/hooks/index.ts
@@ -1,8 +1,9 @@
 export { default as useCalendarData } from './useCalendarData';
+export { default as useColor } from './useColor';
+export { default as useLocalStorage } from './useLocalStorage';
+export { default as useNavItems } from './useNavItems';
 export { default as useResize } from './useResize';
 export { default as useSettings } from './useSettings';
 export { default as useSubjects } from './useSubjects';
 export { default as useTimeCodes } from './useTimeCodes';
 export { default as useUserData } from './useUserData';
-export { default as useColor } from './useColor';
-export { default as useNavItems } from './useNavItems';
