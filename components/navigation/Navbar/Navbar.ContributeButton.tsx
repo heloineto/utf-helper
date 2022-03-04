eb59da54fe6004e9a334097120e136ae1bf21dcf
Heloi Neto <hey@heloineto.com>
2022-03-04 13:57:04 -0300

    updated NavbarContributeButton

--- components/navigation/Navbar/Navbar.ContributeButton.tsx
+++ components/navigation/Navbar/Navbar.ContributeButton.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const NavbarContributeButton = (props: Props) => {
   return (
     <a
-      className="h-7 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/10 rounded-full px-3 flex items-center hover:bg-sky-400/20"
+      className="group h-7 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/10 rounded-full px-3 flex items-center hover:bg-sky-400/20"
       target="_blank"
       href="https://github.com/heloineto/utf-helper"
       rel="noreferrer"
@@ -22,7 +22,7 @@ const NavbarContributeButton = (props: Props) => {
       </svg>
       <span className="ml-2">Criado por Heloi Neto - Contribua no Github!</span>
       <CaretRight
-        className="h-3 w-auto ml-3 overflow-visible text-sky-300 dark:text-sky-400"
+        className="group-hover:ml-4 transition-all h-3 w-auto ml-3 overflow-visible text-sky-300 dark:text-sky-400"
         weight="bold"
       />
     </a>
