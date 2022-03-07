caac033673285b46a6c0e2a6498c343b5431850d
Heloi Neto <hey@heloineto.com>
2022-03-06 21:13:37 -0300

    updated NavbarGoogleSignIn

--- components/navigation/Navbar/Navbar.GoogleSignIn.tsx
+++ components/navigation/Navbar/Navbar.GoogleSignIn.tsx
@@ -10,16 +10,13 @@ const NavbarGoogleSignIn = ({ className, ...muiButtonProps }: Props) => {
   const signInWithGoogle = useSignInWithGoogle();
 
   return (
-    <Tooltip title="Entre com o google salvar seus dados" arrow>
-      <PrimaryButton
-        size="small"
-        className={classNames(className, 'w-40')}
-        onClick={signInWithGoogle}
-        {...muiButtonProps}
-      >
-        Entar
-        <GoogleLogomark className="ml-2" />
-      </PrimaryButton>
+    <Tooltip title="Entre com o Google Salvar seus Dados" arrow>
+      <div className={classNames(className, 'w-40')}>
+        <PrimaryButton size="small" onClick={signInWithGoogle} {...muiButtonProps}>
+          Entar
+          <GoogleLogomark className="ml-2" />
+        </PrimaryButton>
+      </div>
     </Tooltip>
   );
 };
