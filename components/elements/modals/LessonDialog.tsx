8cd24b9687ecef7a5b1a8a8ca2892ffd01f509e8
Heloi Neto <hey@heloineto.com>
2022-03-09 19:27:42 -0300

    updated LessonDialog

--- components/elements/modals/LessonDialog.tsx
+++ components/elements/modals/LessonDialog.tsx
@@ -24,7 +24,7 @@ const LessonDialog = ({ lesson, onClose, ...muiDialogProps }: Props) => {
 
   return (
     <CustomDialog onClose={onClose} {...muiDialogProps}>
-      <div className="px-6 py-8 flex items-center justify-start gap-x-3 border-b border-slate-200 dark:border-slate-700">
+      <div className="px-3 py-4 sm:px-6 sm:py-8 flex items-center justify-start gap-x-3 border-b border-slate-200 dark:border-slate-700">
         <div
           className="rounded h-14 w-20 flex-shrink-0 pt-full ring-1 ring-inset ring-slate-900/5 dark:ring-0 dark:highlight-white/10 flex flex-col justify-center items-center text-sm font-bold"
           style={{
@@ -38,22 +38,26 @@ const LessonDialog = ({ lesson, onClose, ...muiDialogProps }: Props) => {
           <div>{classObject.code}</div>
         </div>
         <div>
-          <div className="text-slate-900 dark:text-slate-100 text-xl">
+          <div className="text-slate-900 dark:text-slate-100 text-lg sm:text-xl">
             {classObject.subjectName}
           </div>
           <div className="flex justify-start items-center gap-x-2">
-            <div className="text-slate-900 dark:text-slate-100">
+            <div className="text-slate-900 dark:text-slate-100 text-sm sm:text-base">
               {isInPerson ? (
                 <div className="underline">Aula Presencial</div>
               ) : (
                 <div>Aula Remota {lesson.isSync ? 'Síncrona' : 'Assíncrona'}</div>
               )}
             </div>
-            {dateStr && <div className="leading-6 dark:text-slate-500">{dateStr}</div>}
+            {dateStr && (
+              <div className="leading-6 dark:text-slate-500 text-xs sm:text-base">
+                {dateStr}
+              </div>
+            )}
           </div>
         </div>
       </div>
-      <div className="px-6 py-8">
+      <div className="px-3 py-4 sm:px-6 sm:py-8">
         <dl className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4">
           <div className="col-span-2 sm:col-span-4">
             <dt className="text-sm text-slate-500 dark:text-slate-400">
@@ -63,6 +67,14 @@ const LessonDialog = ({ lesson, onClose, ...muiDialogProps }: Props) => {
               {lesson?.description}
             </dd>
           </div>
+          {lesson?.scheduleCell?.locationCodes && (
+            <div className="sm:col-span-1">
+              <dt className="text-sm text-slate-500 dark:text-slate-400">Localização</dt>
+              <dd className="mt-1 text-sm font-medium text-slate-900 dark:text-slate-100">
+                {lesson?.scheduleCell?.locationCodes.join(' ou ')}
+              </dd>
+            </div>
+          )}
           <div className="sm:col-span-1">
             <dt className="text-sm text-slate-500 dark:text-slate-400">Data Prevista</dt>
             <dd className="mt-1 text-sm font-medium text-slate-900 dark:text-slate-100">
@@ -103,7 +115,7 @@ const LessonDialog = ({ lesson, onClose, ...muiDialogProps }: Props) => {
           </div>
         </dl>
       </div>
-      <div className="px-6 py-8 border-t border-slate-200 dark:border-slate-700 flex justify-between flex-row gap-5 ">
+      <div className="px-3 py-4 sm:px-6 sm:py-8 border-t border-slate-200 dark:border-slate-700 flex justify-between flex-row gap-5 ">
         {classObject?.paCode && (
           <a
             href={`https://sistemas2.utfpr.edu.br/dpls/sistema/aluno06/mpPlanejamentoAula.pcPlanejFinalizado?p_turmidvc=${classObject.paCode}&p_print=1`}
