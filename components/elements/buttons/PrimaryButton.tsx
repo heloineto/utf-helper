777748e8a8be64264f9ff881bf1a36c52e0f2fa2
Heloi Neto <hey@heloineto.com>
2022-03-11 14:04:43 -0300

    updated PrimaryButton

--- components/elements/buttons/PrimaryButton.tsx
+++ components/elements/buttons/PrimaryButton.tsx
@@ -1,23 +1,40 @@
+import { styled } from '@mui/material/styles';
 import { SettingsContext } from '@lib/context';
 import { Button, ButtonProps } from '@mui/material';
 import classNames from 'clsx';
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import twColors from 'tailwindcss/colors';
 
 interface Props extends ButtonProps {
   colorName?: keyof typeof twColors;
 }
 
-const PrimaryButton = ({ className, colorName, ...muiButtonProps }: Props) => {
-  const color = twColors[colorName ?? 'sky'] as any;
+const PrimaryButton = ({ className, colorName = 'sky', ...muiButtonProps }: Props) => {
+  const color = twColors[colorName] as any;
   const { darkMode } = useContext(SettingsContext);
 
+  const ColorButton = useMemo(
+    () =>
+      styled(Button)(() => ({
+        backgroundColor: `${color[darkMode ? 700 : 100]} !important`,
+        color: color[darkMode ? 300 : 500],
+        borderColor: color[400],
+        '&:hover': {
+          backgroundColor: `${color[darkMode ? 600 : 200]} !important`,
+          borderColor: color[500],
+          color: color[darkMode ? 200 : 600],
+        },
+      })),
+    [darkMode, color]
+  );
+
   return (
-    <Button
+    <ColorButton
       className={classNames(
         className,
-        'bg-sky-100 hover:bg-sky-200 border-2 dark:border-none border-solid border-sky-400 hover:border-sky-500 text-sky-500 text-base py-2 font-semibold dark:bg-sky-700  dark:hover:bg-sky-600 dark:text-sky-300 dark:hover:text-sky-200 transition-colors duration-500'
+        'border-2 dark:border-none border-solid text-base py-2 font-semibold transition-colors duration-500'
       )}
+      variant="contained"
       {...muiButtonProps}
     />
   );
